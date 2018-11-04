8265351c3c9084f3672062e19efeeb6824330ba9
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2018-11-04 14:26:35 -0700

    Clean up unused code and dependencies

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 require('dotenv').load();
 
 const express = require('express');
-// var async = require('async');
-// var router = express.Router();
 const request = require('request');
 const app = express();
-// const meetup = require('meetup-api')({
-// 	key: process.env.MEETUP_API
-// });
 
 const port = 8080
 
@@ -42,39 +37,6 @@ app.get('/', (req, response) => {
     }
    })
 })
-
-// app.get('/', (req, res) => res.send(
-// 	request(`https://api.meetup.com/find/groups?` + `key=${process.env.MEETUP_API}` + `&zip=11211&radius=1&category=34&order=members`, { json: true }, (err, res, body) => {
-// 	  if (err) { return console.log(err); }
-// 		console.log(process.env.MEETUP_API);
-// 	  console.log(body);
-// 	})
-// ))
-
-// console.log('Server running!');
-// router.get('/', indexRoute);
-
-// module.exports = router;
-
-// function indexRoute(req, res, next) {
-// 	async.parallel({ group: getGroup, events: getEvents }, renderIndex.bind(null, res, next));
-// }
-//
-// function getGroup(callback) {
-// 	meetup.getGroup({urlname: process.env.MEETUP_GROUP_NAME, fields: 'group_photo'}, callback);
-// }
-//
-// function getEvents(callback) {
-// 	meetup.getEvents({'group_urlname': process.env.MEETUP_GROUP_NAME}, callback);
-// }
-//
-// function renderIndex(res, next, err, results) {
-// 	if (err) {
-// 		return next(err);
-// 	}
-//
-// 	res.render('index', { group: results.group, events: results.events.results });
-// }
 
 // const getEvents = request(`https://api.meetup.com/find/groups?` + `key=${process.env.MEETUP_API}` + `&zip=11211&radius=1&category=34&order=members`, { json: true }, (err, res, body) => {
 //   if (err) { return console.log(err); }
